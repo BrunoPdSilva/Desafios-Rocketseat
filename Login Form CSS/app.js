658c9a6052ae98fd8de5f507795634783063a3d5
Bruno Peres <bruno_16515@hotmail.com>
2022-09-08 17:31:06 -0300

    Desktop - Login Form finalizado

---
+++ Login Form CSS/app.js
@@ -0,0 +1,11 @@
+/* const checkbox = document.querySelector('#checkbox');
+const checkboxLabel = document.querySelector('#checkbox-label');
+
+checkbox.addEventListener('change', ({ target }) => {
+  if (target.checked) {
+    console.log('Check')
+    checkboxLabel.style.backgroundColor = '#ffc632'
+  } else {
+    console.log('Disabled')
+  }
+}) */
