81a3402e5fe2ab7f9e6df06aba2c5f4d0c98b9ae
Bruno Peres <bruno_16515@hotmail.com>
2022-09-09 10:08:42 -0300

    Mostrar e esconder senha do form funcionando

---
+++ Login Form CSS/app.js
@@ -0,0 +1,14 @@
+const passwordInput = document.querySelector('input[type="password"]')
+const eye = document.querySelector('#eye');
+
+eye.addEventListener('click', () => {
+  const inputType = passwordInput.attributes.type.value;
+
+  if (inputType === 'password') {
+    eye.setAttribute('src', './images/eye-focus.svg');
+    passwordInput.setAttribute('type', 'text');
+  } else {
+    eye.setAttribute('src', './images/eye.svg');
+    passwordInput.setAttribute('type', 'password');    
+  }
+})
