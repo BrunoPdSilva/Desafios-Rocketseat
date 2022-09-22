8d1b0c43b8f66c116b16f8a731be4938f7a6ca87
Bruno Peres <bruno_16515@hotmail.com>
2022-09-22 14:53:00 -0300

    :iphone: Menu responsivo adicionado

--- RocketBlog/app.js
+++ RocketBlog/app.js
@@ -1,10 +1,20 @@
-const linksList = document.querySelectorAll('.list');
+const body = document.querySelector("body");
+const header = document.getElementById("navHeader");
+const burgerMenu = document.querySelector(".burguer-menu");
+const linksList = document.querySelectorAll(".list");
+
+function toggleMenu() {
+  header.classList.toggle("active");
+  body.classList.toggle("active");
+}
 
 function activeLink() {
   linksList.forEach(item => {
-    item.classList.remove('nav-active');
-    this.classList.add('nav-active');
-  })
+    item.classList.remove("nav-active");
+    this.classList.add("nav-active");
+  });
 }
 
-linksList.forEach(item => item.addEventListener('click', activeLink))
+linksList.forEach(item => item.addEventListener("click", activeLink));
+
+burgerMenu.addEventListener("click", toggleMenu);
