78669713364ea07bbc7dcca51dc5b698b1ac6153
Bruno Peres <bruno_16515@hotmail.com>
2022-09-23 11:23:53 -0300

    :iphone: Responsividade finalizada

--- RocketBlog/app.js
+++ RocketBlog/app.js
@@ -1,11 +1,18 @@
 const body = document.querySelector("body");
 const header = document.getElementById("navHeader");
 const burgerMenu = document.querySelector(".burguer-menu");
+const menuIcon = document.querySelector(".menu-icon");
 const linksList = document.querySelectorAll(".list");
 
 function toggleMenu() {
   header.classList.toggle("active");
   body.classList.toggle("active");
+
+  if (menuIcon.attributes.src.value === "./assets/menu-burger.svg") {
+    menuIcon.setAttribute("src", "./assets/cross.svg");
+  } else if (menuIcon.attributes.src.value === "./assets/cross.svg" ) {
+    menuIcon.setAttribute("src", "./assets/menu-burger.svg");
+  }
 }
 
 function activeLink() {
