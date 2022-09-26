9a0f4042e628b41dbc97a07eb7e01c437e3f1ac6
Bruno Peres <bruno_16515@hotmail.com>
2022-09-26 16:32:10 -0300

    ✨ Botão de sortear filmes funcionando

--- Rocketflix/main.js
+++ Rocketflix/main.js
@@ -1,28 +1,34 @@
 import { BASE_URL, API_KEY, IMG_URL, language } from "./api.mjs";
 
-const filmContainer = document.querySelector('#film')
+const filmContainer = document.querySelector("#film");
+const buttonRandomFilm = document.querySelector("button");
 
-const randomFilm = (Math.random() * 1000).toFixed(0)
+function getData() {
+  const randomFilm = (Math.random() * 1000).toFixed(0);
 
-const getData = () => {
   fetch(`${BASE_URL + randomFilm}?api_key=${API_KEY}&${language}`)
     .then(response => response.json())
-    .then(data => renderFilm(data))
+    .then(data => renderFilm(data));
 }
 
-const renderFilm = (film) => {
-
-  filmContainer.innerHTML = `
-  <img src=${IMG_URL + film.poster_path} alt="Film Poster" id="filmPoster">
-
-  <div class="details-container">
-    <h2 id="filmTitle">${film.title}</h2>
-    <p id="description">${film.overview}</p>
-  </div>
-  `
-  
+function renderFilm(film) {
+  if (film.title) {
+    filmContainer.innerHTML = `
+    <img src=${IMG_URL + film.poster_path} alt="Film Poster" id="filmPoster">
+    <div class="details-container">
+      <h2 id="filmTitle">${film.title}</h2>
+      <p id="description">${film.overview}</p>
+    </div>
+    `;
+  } else if (film.title === undefined) {
+    filmContainer.innerHTML = `
+    <img src="./assets/Poster.png" alt="Film Poster" id="filmPoster">
+    <div class="details-container">
+      <h2 id="filmTitle">Ops, hoje não é dia de assistir filme.
+      Bora codar! 🚀</h2>
+    </div>
+    `;
+  }
 }
 
-getData()
-
-// film.title, film.overview, film.poster_path
+buttonRandomFilm.addEventListener("click", getData);
