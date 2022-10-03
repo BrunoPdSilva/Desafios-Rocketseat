bd54824eccb9c20e4009ae3ba864dcfe73594946
Bruno Peres <bruno_16515@hotmail.com>
2022-10-03 11:44:41 -0300

    :iphone: Rocketshoes - Responsividade finalizada

---
+++ RocketShoes/app.js
@@ -0,0 +1,19 @@
+const openModalButton = document.querySelector(".burgerIcon");
+
+const modal = document.querySelector(".navLinks")
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+
+function handleModal() {
+  modal.classList.toggle("active");
+  main.classList.toggle("hidden");
+  footer.classList.toggle("hidden");
+
+  if (openModalButton.attributes.src.value === "./assets/menuIcon.svg") {
+    openModalButton.setAttribute('src', "./assets/cross.svg")
+  } else { 
+    openModalButton.setAttribute('src', "./assets/menuIcon.svg")
+  }
+}
+
+openModalButton.addEventListener("click", handleModal);
