1f042cc0fd6bbc943728ab4503c014db157dde4f
Bruno Peres <bruno_16515@hotmail.com>
2023-07-07 14:32:20 -0300

    :sparkles: Auto resize textarea

--- Twitter_Clone/src/components/Tweet.tsx
+++ Twitter_Clone/src/components/Tweet.tsx
@@ -1,6 +1,6 @@
 import { ChatCircle, ArrowsClockwise, Heart } from "phosphor-react";
-import { useEffect, useRef, useState } from "react";
 import { TweetAction } from "./TweetAction";
+import TextareaAutosize from "react-textarea-autosize";
 
 interface TweetProps {
   name: string;
@@ -10,18 +10,6 @@ interface TweetProps {
 }
 
 export function Tweet({ name, user, image, text }: TweetProps) {
-  const [state] = useState(text);
-
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    const { current } = textAreaRef;
-
-    if (current) {
-      current.style.height = current.scrollHeight + "px";
-    }
-  }, [state]);
-
   return (
     <section className="w-full border-y-[1px] border-blue-100 px-5 py-6 flex gap-3">
       <img src={image} alt="User Picture" className="w-14 h-14 rounded-full" />
@@ -32,11 +20,10 @@ export function Tweet({ name, user, image, text }: TweetProps) {
           <span className="text-sm text-blue-200 font-medium">{user}</span>
         </h2>
 
-        <textarea
+        <TextareaAutosize
+          value={text}
+          className="resize-none outline-none"
           readOnly
-          className="w-full resize-none outline-none"
-          value={state}
-          ref={textAreaRef}
         />
 
         <footer className="h-9 pt-3 flex gap-12">
