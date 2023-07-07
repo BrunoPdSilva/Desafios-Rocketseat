c98465218fb67824be6c777d4b36ed0fbc958c1a
Bruno Peres <bruno_16515@hotmail.com>
2023-07-07 10:20:03 -0300

    :tada: Primeiro commit

---
+++ Twitter_Clone/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      primary: "#1DA1F2",
+      white: "#FFFFFF",
+      black: "#000000",
+      blue: {
+        50: "#F7F9FA",
+        100: "#EBEEF0",
+        200: "#89A2B8",
+        300: "#5B7083",
+        400: "#0F1419",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
